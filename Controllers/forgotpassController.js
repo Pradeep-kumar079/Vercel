09f36f6864aabc1d0206db806821bd768c092eff
Pradeep-kumar079 <pradeepk9348@gmail.com>
2025-11-07 15:26:06 +0530

    ok

--- Controllers/forgotpassController.js
+++ Controllers/forgotpassController.js
@@ -21,8 +21,8 @@ exports.forgotPassword = async (req, res) => {
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        user: "pradeepk9348@gmail.com",
+        pass:  "kycjndaberbichuz",
       },
     });
 
