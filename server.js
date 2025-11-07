c84fb4b82dbda8579baf55913b36481fb55810b0
Pradeep-kumar079 <pradeepk9348@gmail.com>
2025-11-07 13:41:27 +0530

    ok

--- server.js
+++ server.js
@@ -74,9 +74,10 @@ const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "client", "build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"));
-  });
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
+
 }
 
 // ✅ Socket.io setup
